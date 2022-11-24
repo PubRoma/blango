24574f8790889d3ab476ec138896cefd0ef3d689
Roman Yastrebtsov <y.rmn@icloud.com>
2022-11-24 20:55:05 +0000

    Finish throttling

--- blango/settings.py
+++ blango/settings.py
@@ -99,6 +99,19 @@ class Dev(Configuration):
     "DEFAULT_PERMISSION_CLASSES": [
       "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "blog.api.throttling.AnonSustainedThrottle",
+        "blog.api.throttling.AnonBurstThrottle",
+        "blog.api.throttling.UserSustainedThrottle",
+        "blog.api.throttling.UserBurstThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon_sustained": "500/day",
+        "anon_burst": "10/minute",
+        "user_sustained": "5000/day",
+        "user_burst": "100/minute",
+    },
+
   }
   
   SWAGGER_SETTINGS = {
@@ -107,7 +120,6 @@ class Dev(Configuration):
         "Basic": {"type": "basic"},
     }
   }
-
 
   INTERNAL_IPS = ['192.168.10.93']
 
