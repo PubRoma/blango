5dc2223477861f77f12c3f0155640432af5b356b
Roman Yastrebtsov <y.rmn@icloud.com>
2022-11-26 00:39:47 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -79,6 +79,7 @@ class Dev(Configuration):
       'rest_framework.authtoken',\
       'drf_yasg',
       'django_filters',
+      'versatileimagefield',
   ]
 
   MIDDLEWARE = [
@@ -133,6 +134,9 @@ class Dev(Configuration):
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
   }
+
+  MEDIA_ROOT = BASE_DIR / "media"
+  MEDIA_URL = "/media/"
 
 
   INTERNAL_IPS = ['192.168.10.93']
